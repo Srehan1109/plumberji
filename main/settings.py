ab13a662573f3e174037b1b1d2802f932669db43
vikas <guptavikas9229@gmail.com>
2022-10-19 22:58:53 +0530

    allowed hosst

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z!jj(8(&e7$gl6!cy^ym51@^-bk#!wsl%$%(8rj+e2hz086jxh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
